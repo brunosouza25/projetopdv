d6786c880fccb208e05cf054b2a2fdd0d4119b80
Bruno Souza <brunopw25@gmail.com>
2020-05-04 13:21:34 -0300

    controle de acesso implementado

--- WindowsFormsApp2/Principal/View/TelaDeLogin.cs
+++ WindowsFormsApp2/Principal/View/TelaDeLogin.cs
@@ -13,22 +13,27 @@ namespace WindowsFormsApp2
         DadosTableAdapters.FuncionarioTableAdapter funcionario = new DadosTableAdapters.FuncionarioTableAdapter();
         public void validar(string login, string senha)
         {
+            
             var func = funcionario.verificarFunc(login);
-            string name, pass;
-            name = func[0]["nomeFunc"].ToString();
-            pass = func[0]["senha"].ToString();
-            if (login == name && senha == pass)
+            string login2, pass;
+            if (func.Count > 0)
             {
-                this.Visible = false;
-                tela.ShowDialog();
-                Close();
+                pass = func[0]["senha"].ToString();
+                if (senha == pass)
+                {
+                    this.Visible = false;
+                    tela.ShowDialog();
+                    Close();
+                }
+                else
+                    MessageBox.Show("Login ou senha errados");
             }
             else
                 MessageBox.Show("Login ou senha errados");
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            validar(TxtBoxPass.Text, TxtBoxUser.Text);
+            validar(TxtBoxUser.Text, TxtBoxPass.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -43,7 +48,7 @@ namespace WindowsFormsApp2
         {
             if (e.KeyCode == Keys.Enter)
             {
-                validar(TxtBoxPass.Text, TxtBoxUser.Text);
+                validar(TxtBoxUser.Text, TxtBoxPass.Text);
             }
         }
     }
